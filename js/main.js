1eb225920839f3d58f0ed85cb7d961c2c4202e9c
khongor <khongor.enkhbold@gmail.com>
2013-03-12 11:01:31 -0500

    Update main.js
    Removed unnecessary functions

--- js/main.js
+++ js/main.js
@@ -5,89 +5,10 @@ $(document).ready(function() {
 		$.ajax({
 			type: 'GET',
 			url: handler + "?search=" + encodeURI(searchKey),
-			dataType: "json"/*,
-			success: renderList*/
+			dataType: "json"
 		});
 	}
 
-	function findById(id) {
-		$.ajax({
-			type: 'GET',
-			url: rootURL + '/' + id,
-			dataType: "json",
-			success: function(data) {
-				$('#btnDelete').show();
-				renderDetails(data);
-			}
-		});
-	}
-
-	function addWine() {
-		console.log('addWine');
-		$.ajax({
-			type: 'POST',
-			contentType: 'application/json',
-			url: rootURL,
-			dataType: "json",
-			data: formToJSON(),
-			success: function(data, textStatus, jqXHR) {
-				alert('Wine created successfully');
-				$('#btnDelete').show();
-				$('#wineId').val(data.id);
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				alert('addWine error: ' + textStatus);
-			}
-		});
-	}
-
-	function updateWine() {
-		$.ajax({
-			type: 'PUT',
-			contentType: 'application/json',
-			url: rootURL + '/' + $('#wineId').val(),
-			dataType: "json",
-			data: formToJSON(),
-			success: function(data, textStatus, jqXHR) {
-				alert('Wine updated successfully');
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				alert('updateWine error: ' + textStatus);
-			}
-		});
-	}
-
-	function deleteWine() {
-		console.log('deleteWine');
-		$.ajax({
-			type: 'DELETE',
-			url: rootURL + '/' + $('#wineId').val(),
-			success: function(data, textStatus, jqXHR) {
-				alert('Wine deleted successfully');
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				alert('deleteWine error');
-			}
-		});
-	}
-
-// Helper function to serialize all the form fields into a JSON string
-	function formToJSON() {
-		return JSON.stringify({
-			"id": $('#id').val(),
-			"name": $('#name').val(),
-			"grapes": $('#grapes').val(),
-			"country": $('#country').val(),
-			"region": $('#region').val(),
-			"year": $('#year').val(),
-			"description": $('#description').val()
-		});
-	}
-
-
-
-	
-	
 	var app = {};
 
 	var editing = false;
